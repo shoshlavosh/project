b519a1efa4a57b88a25c44dac06692a704e4e59e
shoshlavosh <shoshlavosh@gmail.com>
2021-08-16 23:10:33 +0000

    Initial commit of mostly empty files

---
+++ model.py
@@ -0,0 +1,10 @@
+"""Models for tenant helper app"""
+
+#import the SQLAlchemy constructor function
+from flask_sqlalchemy import SQLAlchemy
+
+#calling the constructor function, creating an instance of 
+#SQLAlchemy & assigning it to the variable "db"
+db = SQLAlchemy()
+
+class Own
---
+++ seed_database.py
@@ -0,0 +1,6 @@
+"""Seeds the database"""
+
+import os
+import json
+#open up json file and parse through it, item by item
+#figure out how to connect it to model.py
