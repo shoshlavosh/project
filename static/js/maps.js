45fdbc83a05c0129080360d267adbca58745a17e
shoshlavosh <shoshlavosh@gmail.com>
2021-09-09 00:14:15 +0000

    Added map that shows the individual building's address to building details page

--- static/js/maps.js
+++ static/js/maps.js
@@ -14,9 +14,30 @@ function initMap() {
     }
   );
  
-  const sfMarker = new google.maps.Marker({
-    position: sfCityCoords,
-    title: "San Francisco",
-    map: basicMap
+  // const sfMarker = new google.maps.Marker({
+  //   position: sfCityCoords,
+  //   title: "San Francisco",
+  //   map: basicMap
+  // });
+
+  const geocoder = new google.maps.Geocoder();
+  geocoder.geocode({address: $("#address")[0].innerText}, (results, status) => {
+    console.log($("#address")[0].innerText);
+    if (status === 'OK') {
+      const buildingLocation = results[0].geometry.location;
+
+      console.log(buildingLocation)
+
+      const buildingLocationMarker = new google.maps.Marker({
+        position: buildingLocation,
+        map: basicMap
+      });
+
+      basicMap.setCenter(buildingLocation);
+      basicMap.setZoom(15);
+    } else {
+      alert(`Geocode was unsuccessful for the following reason:${status}`);
+    }
   });
+
 }
